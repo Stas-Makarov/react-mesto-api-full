d68765bcbd822be8d0c1857062f52175f78d43c7
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-15 22:15:02 +0300

    fix styles

--- backend/errors/NoRightsError.js
+++
@@ -1,9 +0,0 @@
-class ErrorNoRights extends Error {
-  constructor(message) {
-    super(message);
-    this.errorMessage = message;
-    this.statusCode = 403;
-  }
-}
-
-module.exports = ErrorNoRights;
