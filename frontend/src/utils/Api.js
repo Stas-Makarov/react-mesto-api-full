7cde9c07ef6a6fbd30f046b9d629276eb16b5e69
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-20 10:22:41 +0300

    fix styles

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,7 +1,6 @@
 class Api {
-    constructor(settings) {
-      this._baseUrl = settings.baseUrl;
-      this._headers = settings.headers;
+    constructor(options) {
+      this._options = options;
       }
   
     _checkResponse(res) {
@@ -12,52 +11,58 @@ class Api {
     }
   
     getInitialCards() {
-      return fetch(`${this._baseUrl}cards`, {
+      return fetch(`${this._options.baseUrl}cards`, {
         method: 'GET',
-        headers: this._headers
+        headers: this._options.headers,
+        credentials: 'include',
       })
-      .then(this._checkResponse);
+      .then(res => this._checkResponse(res));
     }
   
     getUserInfo() {
-      return fetch(`${this._baseUrl}users/me`, {
+      return fetch(`${this._options.baseUrl}users/me`, {
         method: 'GET',
-        headers: this._headers
+        headers: this._options.headers,
+        credentials: 'include',
       })
-      .then(this._checkResponse); 
+      .then(res => this._checkResponse(res)); 
     }
   
     updateUserInfo(data) {
-      return fetch(`${this._baseUrl}users/me`, {
+      return fetch(`${this._options.baseUrl}users/me`, {
         method: 'PATCH',
-        headers: this._headers,
-        body: JSON.stringify(data)
+        headers: this._options.headers,
+        body: JSON.stringify(data),
+        credentials: 'include'
       })
-      .then(this._checkResponse);
+      .then(res => this._checkResponse(res));
     }
   
     addNewCard(data) {
-      return fetch(`${this._baseUrl}cards`, {
+      return fetch(`${this._options.baseUrl}cards`, {
         method: 'POST',
-        headers: this._headers,
-        body: JSON.stringify(data) 
+        headers: this._options.headers,
+        body: JSON.stringify(data),
+        credentials: 'include' 
       })
-      .then(this._checkResponse);
+      .then(res => this._checkResponse(res));
     }
   
     changeLikeCardStatus(id, isLiked) {
-      return fetch(`${this._baseUrl}cards/likes/${id}`, {
+      return fetch(`${this._options.baseUrl}cards/likes/${id}`, {
         method: `${isLiked ? 'DELETE' : 'PUT'}`,
-        headers: this._headers
+        headers: this._options.headers,
+        credentials: 'include',
       })
-      .then(this._checkResponse);
+      .then(res => this._checkResponse(res));
     }
   
     editAvatar(data) {
-      return fetch(`${this._baseUrl}users/me/avatar`, {
+      return fetch(`${this._options.baseUrl}users/me/avatar`, {
         method: 'PATCH',
-        headers: this._headers,
-        body: JSON.stringify(data)
+        headers: this._options.headers,
+        body: JSON.stringify(data),
+        credentials: 'include',
       })
       .then(this._checkResponse);
     }
@@ -67,7 +72,7 @@ class Api {
         method: 'DELETE',
         headers: this._headers
       })
-      .then(this._checkResponse);
+      .then(res => this._checkResponse(res));
     }
   }
 
