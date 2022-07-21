e838165f31b57f4601ac688c96c5beca7ef6cdcd
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-21 08:20:09 +0300

    fix styles

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,8 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
   'localhost:3000',
   'http://localhost:3000',
   'https://s.d.domainname.students.nomoredomains.xyz',
