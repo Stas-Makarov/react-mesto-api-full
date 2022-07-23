050120cc0b90b5580d5d7d41895587933a2e4fa8
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-23 22:58:32 +0300

    fix styles

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -27,21 +27,21 @@ class Api {
       .then(this._checkResponse); 
     }
   
-    updateUserInfo(data) {
+    updateUserInfo(name, about) {
       return fetch(`${this._baseUrl}/users/me`, {
         method: 'PATCH',
         headers: this._headers,
-        body: JSON.stringify({name: data.name, about: data.about}),
+        body: JSON.stringify({ name, about }),
         credentials: 'include',
       })
       .then(this._checkResponse);
     }
   
-    addNewCard(data) {
+    addNewCard(name, link) {
       return fetch(`${this._baseUrl}/cards`, {
         method: 'POST',
         headers: this._headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify({ name, link }),
         credentials: 'include',
       })
       .then(this._checkResponse);
@@ -56,11 +56,11 @@ class Api {
       .then(this._checkResponse);
     }
   
-    editAvatar(data) {
+    editAvatar(avatar) {
       return fetch(`${this._baseUrl}/users/me/avatar`, {
         method: 'PATCH',
         headers: this._headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify(avatar),
         credentials: 'include',
       })
       .then(this._checkResponse);
