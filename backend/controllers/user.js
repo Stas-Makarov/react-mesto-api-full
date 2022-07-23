1acff1b4eee983f041290f04eec4278e01185dae
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-23 21:49:41 +0300

    fix styles

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -105,7 +105,7 @@ module.exports.login = (req, res, next) => {
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
-      res.status(200).send({ data: user });
+      res.status(200).send(user);
     })
     .catch(next);
 };
