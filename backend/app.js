7b95b8b8e33944ff315076cde1e22250afce8808
Stas Makarov <makarov.s.d@yandex.ru>
2022-07-23 09:19:40 +0300

    fix styles

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,10 @@ app.use(
   }),
 );
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
